b955b03f31acc9242b92eba09a86442ab935e338
Tony <at5393-5394@yahoo.com.hk>
2018-04-21 01:01:46 +0800

    Remove option menu in MainActivity

--- app/src/main/java/au/edu/uow/fyp01/abas/MainActivity.java
+++ app/src/main/java/au/edu/uow/fyp01/abas/MainActivity.java
@@ -18,7 +18,6 @@ import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
-
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
@@ -26,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
     Toolbar toolbar = findViewById(R.id.toolbar);
     setSupportActionBar(toolbar);
 
+    //TODO Replace with search beacon
     FloatingActionButton fab = findViewById(R.id.fab);
     fab.setOnClickListener(new View.OnClickListener() {
       @Override
@@ -55,28 +55,13 @@ public class MainActivity extends AppCompatActivity {
     }
   }
 
+  /**
+   * Do not create option menu in app bar/action bar
+   */
   @Override
   public boolean onCreateOptionsMenu(Menu menu) {
-    // Inflate the menu; this adds items to the action bar if it is present.
-    getMenuInflater().inflate(R.menu.main, menu);
-    return true;
+    return false;
   }
-
-  @Override
-  public boolean onOptionsItemSelected(MenuItem item) {
-    // Handle action bar item clicks here. The action bar will
-    // automatically handle clicks on the Home/Up button, so long
-    // as you specify a parent activity in AndroidManifest.xml.
-    int id = item.getItemId();
-
-    //noinspection SimplifiableIfStatement
-    if (id == R.id.action_settings) {
-      return true;
-    }
-
-    return super.onOptionsItemSelected(item);
-  }
-
 
   @SuppressWarnings("StatementWithEmptyBody")
   private NavigationView.OnNavigationItemSelectedListener onNavigationItemSelectedListener = new OnNavigationItemSelectedListener() {
