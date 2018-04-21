ca54d6b307ccaac2a041b89722057077e2b6d320
Tony <at5393-5394@yahoo.com.hk>
2018-04-21 11:58:01 +0800

    Add navigation drawer icon

--- app/src/main/java/au/edu/uow/fyp01/abas/MainActivity.java
+++ app/src/main/java/au/edu/uow/fyp01/abas/MainActivity.java
@@ -70,17 +70,15 @@ public class MainActivity extends AppCompatActivity {
       // Handle navigation view item clicks here.
       int id = item.getItemId();
 
-      if (id == R.id.nav_camera) {
+      if (id == R.id.nav_search_beacon) {
         // Handle the camera action
-      } else if (id == R.id.nav_gallery) {
+      } else if (id == R.id.nav_file) {
 
-      } else if (id == R.id.nav_slideshow) {
+      } else if (id == R.id.nav_record) {
 
-      } else if (id == R.id.nav_manage) {
+      } else if (id == R.id.nav_setting) {
 
-      } else if (id == R.id.nav_share) {
-
-      } else if (id == R.id.nav_send) {
+      } else if (id == R.id.nav_logout) {
 
       }
       DrawerLayout drawer = findViewById(R.id.drawer_layout);
