bac8ea9a9483b0a94152ed49c8dd432ae9699964
AthensCabal <athenwerccaballero@yahoo.com>
2018-04-23 00:40:39 +0800

    Added firebase database and ui to gradle. Added ClassesListFragment, RecordFragment,StudentListFragment. Modified StudentModel.

---
+++ app/src/main/java/au/edu/uow/fyp01/abas/Fragment/RecordFragment.java
@@ -0,0 +1,33 @@
+package au.edu.uow.fyp01.abas.Fragment;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import au.edu.uow.fyp01.abas.R;
+
+public class RecordFragment extends Fragment {
+
+
+    public RecordFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        if (container != null) {
+            container.removeAllViews();
+        }
+
+        return inflater.inflate(R.layout.fragment_record, container, false);
+
+    }
+
+}
