00b006328f26fbb25ef6605299eebef45e194053
manishmall20 <manish147220@gmail.com>
2018-05-18 06:56:00 +0800

    Fixed Multi Option

--- app/src/main/java/au/edu/uow/fyp01/abas/Activity/FileSharingAdd.java
+++ app/src/main/java/au/edu/uow/fyp01/abas/Activity/FileSharingAdd.java
@@ -1,5 +1,6 @@
 package au.edu.uow.fyp01.abas.Activity;
 
+import android.app.DatePickerDialog;
 import android.content.ContentResolver;
 import android.content.ContentUris;
 import android.content.Context;
@@ -21,12 +22,17 @@ import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
+import android.util.SparseBooleanArray;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.MimeTypeMap;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -47,7 +53,13 @@ import com.google.firebase.storage.UploadTask;
 
 import java.io.File;
 import java.net.URISyntaxException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 
 import au.edu.uow.fyp01.abas.R;
@@ -58,9 +70,12 @@ public class FileSharingAdd extends AppCompatActivity {
     private ImageButton dateImageButton;
     private ImageButton timeImageButton;
 
+    private Button confirmButton;
+    private Button cancelButton;
+
     private TextView fileNameDisplay;
     private TextView fileTypeDisplay;
-    private TextView dateInput;
+    private EditText dateInput;
     private TextView timeInput;
 
     private String nameOfFile;
@@ -71,33 +86,45 @@ public class FileSharingAdd extends AppCompatActivity {
     private RecyclerView allUsersList;
     private Uri resultsURI;
 
+    private List<String> listID;
+    private SparseBooleanArray itemStateArray;
+
     private DatabaseReference allDatabaseUserReference;
     private FirebaseAuth mAuth;
     private FirebaseDatabase mDataBase;
     private FirebaseRecyclerOptions<FileSharingAddUserRetrieveClass> firebaseOptions;
     private FirebaseRecyclerAdapter<FileSharingAddUserRetrieveClass, FileSharingAdd.FileSharingAddViewHolder> firebaseRecyclerAdapter;
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_file_sharing_add);
+
+        itemStateArray= new SparseBooleanArray();
 
         //Initialize the onscreen items and set them
         addFileButton = (ImageButton) findViewById(R.id.activity_file_sharing_add_addFile_button);
         dateImageButton = (ImageButton) findViewById(R.id.activity_file_sharing_image_button_date);
         timeImageButton = (ImageButton) findViewById(R.id.activity_file_sharing_time_image_button);
 
+        confirmButton = (Button) findViewById(R.id.activity_file_sharing_add_confirm_button);
+        cancelButton = (Button) findViewById(R.id.activity_file_sharing_cancel_button);
+
         fileNameDisplay = (TextView) findViewById(R.id.activity_file_sharing_add_filename);
         fileTypeDisplay = (TextView) findViewById(R.id.activity_file_sharing_file_type_view);
-        dateInput = (TextView) findViewById(R.id.activity_file_sharing_date_input);
+        dateInput = (EditText) findViewById(R.id.activity_file_sharing_date_input);
         timeInput = (TextView) findViewById(R.id.activity_file_sharing_time_input);
 
         allUsersList = (RecyclerView) findViewById(R.id.activity_file_sharing_add_recylerview);
 
+        listID  = new ArrayList<String>();
+
         allUsersList.setHasFixedSize(true);
         allUsersList.setLayoutManager(new LinearLayoutManager(FileSharingAdd.this));
 
-        allUsersList.addItemDecoration(new SpacesItemDecoration(3));
+        allUsersList.addItemDecoration(new SpacesItemDecoration(5));
 
 
         filePicked = null;
@@ -108,24 +135,47 @@ public class FileSharingAdd extends AppCompatActivity {
 
         firebaseOptions = new FirebaseRecyclerOptions.Builder<FileSharingAddUserRetrieveClass>().setQuery(allDatabaseUserReference,FileSharingAddUserRetrieveClass.class).build();
 
+
+
+
+
         firebaseRecyclerAdapter =
                 new FirebaseRecyclerAdapter<FileSharingAddUserRetrieveClass, FileSharingAddViewHolder>(firebaseOptions) {
                     @Override
-                    protected void onBindViewHolder(@NonNull FileSharingAddViewHolder holder, final int position, @NonNull FileSharingAddUserRetrieveClass model) {
+                    protected void onBindViewHolder(@NonNull final FileSharingAddViewHolder holder, final int position, @NonNull FileSharingAddUserRetrieveClass model) {
                         holder.setFullName(model.getFullName());
 
-
-                        holder.mView.setOnClickListener(new View.OnClickListener() {
+                        (holder.checkBox).setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
                             @Override
-                            public void onClick(View view) {
+                            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
 
-                                String vist_profile_id = getRef(position).getKey();
-                                upload(view,vist_profile_id);
-                                Intent back = new Intent(FileSharingAdd.this,FileSharingHome.class);
-                                startActivity(back);
+                                if(compoundButton.isChecked()){
+
+                                    compoundButton.setChecked(true);
+                                    listID.add(getRef(position).getKey());
 
 
-                        }});
+                                } else {
+                                    compoundButton.setChecked(false);
+                                    listID.remove(getRef(position).getKey());
+
+                                }
+
+                            }
+                        });
+
+
+//                        holder.mView.setOnClickListener(new View.OnClickListener() {
+//                            @Override
+//                            public void onClick(View view) {
+//
+//                                String vist_profile_id = getRef(position).getKey();
+//                                upload(view,vist_profile_id);
+//                                Intent back = new Intent(FileSharingAdd.this,FileSharingHome.class);
+//                                startActivity(back);
+//
+//
+//                        }});
                         }
 
 
@@ -149,6 +199,26 @@ public class FileSharingAdd extends AppCompatActivity {
                 startActivityForResult(filePickedIntent, file_pick);
             }
         });
+
+        confirmButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Iterator itr = listID.iterator();
+
+                if(listID.isEmpty()){
+                    Toast.makeText(FileSharingAdd.this, "FUCK",Toast.LENGTH_LONG).show();
+                }
+
+                while(itr.hasNext()){
+                    Toast.makeText(FileSharingAdd.this, itr.next().toString().trim(),Toast.LENGTH_LONG).show();
+                }
+
+            }
+        });
+
+
+        setDate fromDate = new setDate(dateInput, this);
 
     }
 
@@ -303,16 +373,22 @@ public class FileSharingAdd extends AppCompatActivity {
 
     public static class FileSharingAddViewHolder extends RecyclerView.ViewHolder {
         View mView;
+        CheckBox checkBox;
+
 
         public FileSharingAddViewHolder(View itemView) {
             super(itemView);
             mView = itemView;
+
         }
 
         public void setFullName(String fullname) {
-            CheckBox nameCheck = (CheckBox) mView.findViewById(R.id.activity_file_sharing_add_username);
-            nameCheck.setText(fullname);
+            checkBox = (CheckBox) mView.findViewById(R.id.activity_file_sharing_add_username);
+            checkBox.setText(fullname);
         }
+
+
+
 
 
     }
@@ -358,6 +434,43 @@ public class FileSharingAdd extends AppCompatActivity {
         }
 
         return extension;
+    }
+
+    class setDate implements View.OnFocusChangeListener, DatePickerDialog.OnDateSetListener {
+
+        private EditText editText;
+        private Calendar myCalendar;
+
+        public setDate(EditText editText, Context ctx){
+            this.editText = editText;
+            this.editText.setOnFocusChangeListener(this);
+            myCalendar = Calendar.getInstance();
+        }
+
+        @Override
+        public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth)     {
+            // this.editText.setText();
+
+            String myFormat = "dd-MM-yyyy"; //In which you need put here
+            SimpleDateFormat sdformat = new SimpleDateFormat(myFormat, Locale.US);
+            myCalendar.set(Calendar.YEAR, year);
+            myCalendar.set(Calendar.MONTH, monthOfYear);
+            myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+
+            editText.setText(sdformat.format(myCalendar.getTime()));
+
+        }
+
+        @Override
+        public void onFocusChange(View v, boolean hasFocus) {
+            // TODO Auto-generated method stub
+            if(hasFocus){
+                new DatePickerDialog(FileSharingAdd.this, this, myCalendar
+                        .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+                        myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        }
+
     }
 
 
