dae7fe074ea88fa980c3c43951026bde1820924e
AthensCabal <athenwerccaballero@yahoo.com>
2018-05-19 17:10:24 +0800

    Comments Fix

--- app/src/main/java/au/edu/uow/fyp01/abas/Activity/CommentListActivity.java
+++ app/src/main/java/au/edu/uow/fyp01/abas/Activity/CommentListActivity.java
@@ -2,6 +2,7 @@ package au.edu.uow.fyp01.abas.Activity;
 
 import android.app.Activity;
 import android.content.DialogInterface;
+import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
@@ -122,8 +123,9 @@ public class CommentListActivity extends Activity {
 
                         // Set up the input
                         final EditText input = new EditText(getApplicationContext());
+                        input.setTextColor(Color.BLACK);
                         // Specify the type of input expected; this, for example, sets the input as a password, and will mask the text
-                        input.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+                        input.setInputType(InputType.TYPE_CLASS_TEXT);
                         builder.setView(input);
 
                         // Set up the buttons
