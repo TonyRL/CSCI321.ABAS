9d1b1e96638367d780a6fc29549813e5ec293010
AthensCabal <athenwerccaballero@yahoo.com>
2018-05-21 23:08:40 +0800

    Fixed sorting for record. Added a function to allow adding of students directly via student ID

--- app/src/main/java/au/edu/uow/fyp01/abas/Activity/AdminAddStudentActivity.java
+++ app/src/main/java/au/edu/uow/fyp01/abas/Activity/AdminAddStudentActivity.java
@@ -13,11 +13,13 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.HashMap;
 import java.util.Map;
 
+import au.edu.uow.fyp01.abas.Model.ListOfStudentsModel;
 import au.edu.uow.fyp01.abas.R;
 
 public class AdminAddStudentActivity extends Activity {
@@ -48,8 +50,8 @@ public class AdminAddStudentActivity extends Activity {
         final EditText adminAddStudentClassNumber = findViewById(R.id.adminAddStudentClassNumber);
         final EditText adminAddStudentID = findViewById(R.id.adminAddStudentID);
 
+
         Button adminAddStudentBtn = findViewById(R.id.adminAddStudentBtn);
-        //onclick
         adminAddStudentBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -69,30 +71,41 @@ public class AdminAddStudentActivity extends Activity {
                                 String sID = adminAddStudentID.getText().toString();
 
                                 //DO CHECKING IF ANY OF THE BOXES ARE EMPTY
-                                if (firstname.matches("")|| lastname.matches("")||
-                                        classnumber.matches("")|| sID.matches("")){
-                                    Toast.makeText(AdminAddStudentActivity.this,"One of more boxes are empty!", Toast.LENGTH_SHORT)
+                                if (firstname.matches("") || lastname.matches("") ||
+                                        classnumber.matches("") || sID.matches("")) {
+                                    Toast.makeText(AdminAddStudentActivity.this, "One of more boxes are empty!", Toast.LENGTH_SHORT)
                                             .show();
                                     return;
-                                }
-                                else {
+                                } else {
                                     //handle user input into database
                                     Map<String, Object> addToDatabase = new HashMap<>();
 
-                                    addToDatabase.put("classname",classname);
-                                    addToDatabase.put("classnumber",classnumber);
-                                    addToDatabase.put("firstname",firstname);
-                                    addToDatabase.put("lastname",lastname);
-                                    addToDatabase.put("sid",sID);
+                                    //For ListOfStudents
+                                    Map<String, Object> addToDatabase2 = new HashMap<>();
+
+                                    addToDatabase.put("classname", classname);
+                                    addToDatabase.put("classnumber", classnumber);
+                                    addToDatabase.put("firstname", firstname);
+                                    addToDatabase.put("lastname", lastname);
+                                    addToDatabase.put("sid", sID);
 
                                     //update children of Student->SchID->ClassID
                                     //move one level down to ->StudentID
                                     dbref.child(sID).updateChildren(addToDatabase);
 
-                                    Toast.makeText(AdminAddStudentActivity.this,"Added to new student to class " + classname, Toast.LENGTH_SHORT)
+                                    addToDatabase2.put("firstname", classnumber);
+                                    addToDatabase2.put("lastname", lastname);
+                                    addToDatabase2.put("sid", sID);
+
+                                    //this points to ListOfStudents->SchID->StudentID
+                                    dbref = db.getReference().child("ListOfStudents").child(schID)
+                                            .child(sID);
+                                    dbref.updateChildren(addToDatabase2);
+
+                                    Toast.makeText(AdminAddStudentActivity.this, "Added new student to class " + classname, Toast.LENGTH_SHORT)
                                             .show();
                                     finish();
-                                 }
+                                }
                             }
                         });
 
@@ -103,14 +116,105 @@ public class AdminAddStudentActivity extends Activity {
                                 dialog.cancel();
                             }
                         });
-
                 AlertDialog alert11 = builder1.create();
                 alert11.show();
                 //end of confirmation
+
             }
             //</editor-fold>
         });
-        //end onclick
+        //end add student
+
+
+        //Add existing student
+        final EditText adminExistingSID = findViewById(R.id.adminExistingSID);
+        final EditText adminExistingClassNumber = findViewById(R.id.adminExistingClassNumber);
+        Button adminAddExistingStudentBtn = findViewById(R.id.adminAddExistingStudentBtn);
+        adminAddExistingStudentBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Ask for user confirmation
+                AlertDialog.Builder builder1 = new AlertDialog.Builder(AdminAddStudentActivity.this);
+                builder1.setMessage("Add new student?");
+                builder1.setCancelable(true);
+
+                builder1.setPositiveButton(
+                        "Yes",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                //GET TEXT FROM INPUT FIRST
+                                final String existingSID = adminExistingSID.getText().toString();
+                                final String existingClassNumber = adminExistingClassNumber.getText().toString();
+
+                                //DO CHECKING IF ANY OF THE BOXES ARE EMPTY
+                                if (existingSID.matches("") || existingClassNumber.matches("")) {
+                                    Toast.makeText(AdminAddStudentActivity.this, "One of more boxes are empty!", Toast.LENGTH_SHORT)
+                                            .show();
+                                    return;
+                                } else {
+                                    FirebaseDatabase db2 = FirebaseDatabase.getInstance();
+                                    DatabaseReference dbref2 = db2.getReference().child("ListOfStudents").child(schID);
+                                    Query query = dbref2.orderByChild("sid").equalTo(existingSID);
+
+                                    query.addListenerForSingleValueEvent(new ValueEventListener() {
+                                        @Override
+                                        public void onDataChange(DataSnapshot dataSnapshot) {
+                                            if (dataSnapshot.exists()) {
+
+                                                for (DataSnapshot node : dataSnapshot.getChildren()) {
+                                                    //School's record of student, not classroom
+                                                    ListOfStudentsModel student1 = node.getValue(ListOfStudentsModel.class);
+
+                                                    //handle user input into database
+                                                    Map<String, Object> addToDatabase = new HashMap<>();
+
+                                                    addToDatabase.put("classname", classname);
+                                                    addToDatabase.put("classnumber", existingClassNumber);
+                                                    addToDatabase.put("firstname", student1.getFirstname());
+                                                    addToDatabase.put("lastname", student1.getLastname());
+                                                    addToDatabase.put("sid", existingSID);
+
+                                                    //update children of Student->SchID->ClassID
+                                                    //move one level down to ->StudentID
+                                                    dbref.child(existingSID).updateChildren(addToDatabase);
+
+                                                    Toast.makeText(AdminAddStudentActivity.this, "Added student to class " + classname, Toast.LENGTH_SHORT)
+                                                            .show();
+                                                    finish();
+                                                }
+
+                                            } else {
+                                                Toast.makeText(AdminAddStudentActivity.this, "Student not found!" + classname, Toast.LENGTH_SHORT)
+                                                        .show();
+                                            }
+                                        }
+
+                                        @Override
+                                        public void onCancelled(DatabaseError databaseError) {
+
+                                        }
+                                    });
+                                }
+
+
+                            }
+                        });
+
+                builder1.setNegativeButton(
+                        "No",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                dialog.cancel();
+                            }
+                        });
+                AlertDialog alert11 = builder1.create();
+                alert11.show();
+                //end of confirmation
+
+
+            }
+        }); // end on click
+
 
     }
 }
