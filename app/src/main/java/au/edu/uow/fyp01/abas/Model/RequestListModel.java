234dffa884497cb4420fcb0101a28f318176c1c1
Athens <athenwerccaballero@yahoo.com>
2018-07-15 17:54:18 +0800

    Added Request List Management

---
+++ app/src/main/java/au/edu/uow/fyp01/abas/Model/RequestListModel.java
@@ -0,0 +1,53 @@
+package au.edu.uow.fyp01.abas.Model;
+
+public class RequestListModel {
+
+  public String fullname;
+  public String title;
+  public String userID;
+  public String staffID;
+
+  public RequestListModel() {}
+
+  public RequestListModel(String fullname,
+      String title, String userID, String staffID){
+
+    this.fullname = fullname;
+    this.title = title;
+    this.userID = userID;
+    this.staffID = staffID;
+
+  }
+
+  public void setFullname(String fullname) {
+    this.fullname = fullname;
+  }
+
+  public String getFullname() {
+    return fullname;
+  }
+
+  public void setUserID(String userID) {
+    this.userID = userID;
+  }
+
+  public String getUserID() {
+    return userID;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setStaffID(String staffID) {
+    this.staffID = staffID;
+  }
+
+  public String getStaffID() {
+    return staffID;
+  }
+}
